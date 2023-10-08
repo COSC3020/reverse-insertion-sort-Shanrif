a4b6afe604f15c8296622f5a9a9efe224be7477a
Shanrif <nriffee@gmail.com>
2023-10-08 14:12:32 -0600

    hm, not sure why this isn't working

--- code.js
+++ code.js
@@ -1,3 +1,38 @@
-function insertionSortReverse(array) {
+function insertionSortReverse(arr) {
+
+    if (arr.length === 0)
+    {
+        return arr;
+    }
+    if (arr.length === 1)
+    {
+        return arr;
+    }
+    // need to make this work for repeat elements
+    repeat = 0; 
+
+
+    for(var i = arr.length; i >= 0; i--) {
+        var val = arr[i];
+        var j;
+        for(j = i; j > 0 && arr[j-1] > val; j--) {
+          arr[j] = arr[j-1];
+        }
+        
+        }
+        arr[j] = val;
+      }
+      return arr;
+
     return array;
+   /* for(var i = 1; i < arr.length; i++) {
+        var val = arr[i];
+        var j;
+        for(j = i; j > 0 && arr[j-1] > val; j--) {
+          arr[j] = arr[j-1];
+        }
+        arr[j] = val;
+      }
+      return arr;
+    return array; */
 }
